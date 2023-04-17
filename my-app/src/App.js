d195525be579d6938bad1e8aa938a5283fb77ef9
MarcinPilarz <130612@student.uni.opole.pl>
2023-04-17 22:14:34 +0200

    MenuUI Added Footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import CarSection from './components/CarSection';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <>
     <CarSection/>
+    <Footer/>
+    </>
   );
 }
 
