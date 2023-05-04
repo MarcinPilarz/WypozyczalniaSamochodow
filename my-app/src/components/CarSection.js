762e12232eb3b18a19522a1f784f14399d08cc89
Mateusz Roman <mateuszroman99@gmail.com>
2023-05-04 13:46:57 +0200

    buttons add buttons to car section

--- my-app/src/components/CarSection.js
+++ my-app/src/components/CarSection.js
@@ -11,30 +11,31 @@ function CarSection() {
     
     </div>
     <div className='cars-container container'>
-        <div className='box'>
+        <div className='box'onClick={()=>console.log('Audi')}>
         <img className='Logo-container' src='./LogoBrandsImg/AudiLogo1.png' alt='Logo Audi'/>
         <h2>AUDI</h2>
         </div>
-        <div className='box'> 
+        <div className='box' onClick={()=>console.log('BMW')}> 
         <img className='Logo-container' src='./LogoBrandsImg/bmwLogo3.png' alt='BMW Logo'/>
         <h2>BMW</h2>
         </div>
-        <div className='box'>
+        <div className='box' onClick={()=>console.log('Honda')}>
             <img className='Logo-container' src='./LogoBrandsImg/HondaLogo.jpg' alt='Lexus Logo' />
             <h2>Honda</h2>
         </div>
     </div>
     <div className='cars-container container'>
 
-        <div className='box'>
+        <div className='box' onClick={()=>console.log('Tesla')}>
         <img  className='Logo-container' src='./LogoBrandsImg/TeslaLogo.png' alt='Logo Tesli'/>
         <h2>Tesla</h2>
         </div>
-        <div className='box'>
+        <div className='box' onClick={()=>console.log('Toyota')}>
         <img className='Logo-container' src='./LogoBrandsImg/ToyotaLogo2.png' alt='Logo Honda'/>
         <h2>Toyota</h2>
         </div>
-        <div className='box'>
+
+        <div className='box' onClick={()=>console.log('volvo')}>
             <img className='Logo-container' src='./LogoBrandsImg/VolvoLogo1.png' alt='VolvoLogo' />
             <h2>Volvo</h2>
         </div>
