77bc7f22bd0156fb5f7227d8ddf12da16dcadb06
MarcinPilarz <130612@student.uni.opole.pl>
2023-05-11 22:57:34 +0200

    develop Security Fixing 2

--- springWebApplication/src/main/java/pl/opole/uni/springWebApp/security/SecurityConfig.java
+++ springWebApplication/src/main/java/pl/opole/uni/springWebApp/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package pl.opole.uni.springWebApp.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +19,7 @@ import pl.opole.uni.springWebApp.services.UserService;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
