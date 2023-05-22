b5830ff4905fd7c6468b0ed1d448ed4c18b84176
Maciek <123741@student.uni.opole.pl>
2023-05-22 13:50:38 +0200

    Added log in function as well as log out

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import '../App.css';
-import Home from './Home';
-
-
-
-import BMWSection from './BMWSection';
 
 function NavBar() {
   const [showPassword, setShowPassword] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [popupLogin, setPopupLogin] = useState(false);
+  const [popupRegister, setPopupRegister] = useState(false);
 
   const handlePasswordToggle = () => {
     setShowPassword(!showPassword);
@@ -27,25 +25,31 @@ function NavBar() {
     }
   };
 
-  const [popupLogin, setPopLogin] = useState(false);
-
   const handleLoginClick = () => {
-    setPopLogin(!popupLogin);
+    setPopupLogin(true);
   };
 
   const closePopUpLogin = () => {
-    setPopLogin(false);
+    setPopupLogin(false);
   };
-  const [popupRegister, setPopRegister] = useState(false);
 
   const handleRegisterClick = () => {
-    setPopRegister(!popupRegister);
+    setPopupRegister(true);
   };
 
   const closePopUpRegister = () => {
-    setPopRegister(false);
+    setPopupRegister(false);
   };
-  
+
+  const handleLoginSubmit = () => {
+    setIsLoggedIn(true);
+    setPopupLogin(false);
+  };
+
+  const handleLogoutClick = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <>
       <header>
@@ -75,111 +79,112 @@ function NavBar() {
             <input type="search" placeholder="Szukaj..." />
           </div>
           <div className="btn-login-container">
-            <button className="btn-login" href="#Logowanie" onClick={handleLoginClick}>
-              Logowanie
-            </button>
-            <button className="btn-login" href="#Rejestracja" onClick={handleRegisterClick}>
-              Rejestracja
-            </button>
+            {isLoggedIn ? (
+              <button className="btn-login" onClick={handleLogoutClick}>
+                Wyloguj
+              </button>
+            ) : (
+              <>
+                <button className="btn-login" href="#Logowanie" onClick={handleLoginClick}>
+                  Logowanie
+                </button>
+                <button className="btn-login" href="#Rejestracja" onClick={handleRegisterClick}>
+                  Rejestracja
+                </button>
+              </>
+            )}
           </div>
         </div>
-{/* Logowanie */}
-        <section className="car-details" id="car-details">
-          <div>
-            {popupLogin ? (
-              <div className="popup-container">
-                <div className="popup">
-                  <div className="popup-header">
-                    <h3 className="popup-header-text">Zaloguj się</h3>
-                    <button className="close-btn" onClick={closePopUpLogin}>
-                      X
-                    </button>
-                  </div>
-                  <div className="popup-inputs-container">
-                    <p className="popup-input-headers">Nazwa użytkownika/Adres email</p>
-                    <input type="text" placeholder="Podaj adres email"></input>
-                    <p className="popup-input-headers">Hasło</p>
+
+        {/* Logowanie */}
+        {popupLogin && (
+          <section className="car-details" id="car-details">
+            <div className="popup-container">
+              <div className="popup">
+                <div className="popup-header">
+                  <h3 className="popup-header-text">Zaloguj się</h3>
+                  <button className="close-btn" onClick={closePopUpLogin}>
+                    X
+                  </button>
+                </div>
+                <div className="popup-inputs-container">
+                  <p className="popup-input-headers">Nazwa użytkownika/Adres email</p>
+                  <input type="text" placeholder="Podaj adres email"></input>
+                  <p className="popup-input-headers">Hasło</p>
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Podaj hasło"
+                    id="passwordInput"
+                  ></input>
+                  <div className="popup-login-show-password">
                     <input
-                      type={showPassword ? 'text' : 'password'}
-                      placeholder="Podaj hasło"
-                      id="passwordInput"
-                    ></input>
-                    <div className="popup-login-show-password">
-                      <input
-                        type="checkbox"
-                        checked={showPassword}
-                        onChange={handlePasswordToggle}
-                      />
-                      <p>Pokaż hasło</p>
-                    </div>
-                    <button className="popup-inputs-button">Zaloguj</button>
+                      type="checkbox"
+                      checked={showPassword}
+                      onChange={handlePasswordToggle}
+                    />
+                    <p>Pokaż hasło</p>
                   </div>
+                  <button className="popup-inputs-button" onClick={handleLoginSubmit}>
+                    Zaloguj
+                  </button>
                 </div>
               </div>
-            ) : (
-              ''
-            )}
-          </div>
-        </section>
+            </div>
+          </section>
+        )}
         {/* Logowanie end */}
+
         {/* Rejestracja */}
-        <section className="car-details" id="car-details">
-          <div>
-            {popupRegister ? (
-              <div className="popup-container">
-                <div className="popup">
-                  <div className="popup-header">
-                    <h3 className="popup-header-text">Zarejestruj się</h3>
-                    <button className="close-btn" onClick={closePopUpRegister}>
-                      X
-                    </button>
-                  </div>
-                  <div className="popup-inputs-container">
+        {popupRegister && (
+          <section className="car-details" id="car-details">
+            <div className="popup-container">
+              <div className="popup">
+                <div className="popup-header">
+                  <h3 className="popup-header-text">Zarejestruj się</h3>
+                  <button className="close-btn" onClick={closePopUpRegister}>
+                    X
+                  </button>
+                </div>
+                <div className="popup-inputs-container">
                   <p className="popup-input-headers">Imię</p>
-                    <input type="text" placeholder="Podaj Imię"></input>
-                    <p className="popup-input-headers">Nazwisko</p>
-                    <input type="text" placeholder="Podaj Nazwisko"></input>
-                    <p className="popup-input-headers">Adres email</p>
-                    <input type="text" placeholder="Podaj adres email"></input>
-                    <p className="popup-input-headers">Nr. kontaktowy</p>
-                    <input type="text" placeholder="Podaj nr. kontaktowy"></input>
-                    <p className="popup-input-headers">Ustaw hasło</p>
+                  <input type="text" placeholder="Podaj Imię"></input>
+                  <p className="popup-input-headers">Nazwisko</p>
+                  <input type="text" placeholder="Podaj Nazwisko"></input>
+                  <p className="popup-input-headers">Adres email</p>
+                  <input type="text" placeholder="Podaj adres email"></input>
+                  <p className="popup-input-headers">Nr. kontaktowy</p>
+                  <input type="text" placeholder="Podaj nr. kontaktowy"></input>
+                  <p className="popup-input-headers">Ustaw hasło</p>
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Podaj hasło"
+                    id="passwordInput"
+                  ></input>
+                  <div className="popup-login-show-password"></div>
+                  <p className="popup-input-headers">Powtórz hasło</p>
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Powtórz hasło"
+                    id="passwordInput"
+                  ></input>
+                  <div className="popup-login-show-password">
                     <input
-                      type={showPassword ? 'text' : 'password'}
-                      placeholder="Podaj hasło"
-                      id="passwordInput"
-                    ></input>
-                    <div className="popup-login-show-password">
-                     
-                    </div>
-                    <p className="popup-input-headers">Powtórz hasło</p>
-                    <input
-                      type={showPassword ? 'text' : 'password'}
-                      placeholder="Powtórz hasło"
-                      id="passwordInput"
-                    ></input>
-                    <div className="popup-login-show-password">
-                      <input
-                        type="checkbox"
-                        checked={showPassword}
-                        onChange={handlePasswordToggle}
-                      />
-                      <p>Pokaż hasło</p>
-                    </div>
-                    <button className="popup-inputs-button">Zarejestruj</button>
+                      type="checkbox"
+                      checked={showPassword}
+                      onChange={handlePasswordToggle}
+                    />
+                    <p>Pokaż hasło</p>
                   </div>
+                  <button className="popup-inputs-button">Zarejestruj</button>
                 </div>
               </div>
-            ) : (
-              ''
-            )}
-          </div>
-        </section>
+            </div>
+          </section>
+        )}
         {/* Rejestracja end */}
       </header>
     </>
   );
-
 }
 
 export default NavBar;
