2a378061eb8c8d69ae1466abcf43b073f211c02b
MarcinPilarz <130612@student.uni.opole.pl>
2023-05-30 21:14:42 +0200

    develop Fxing bugs

--- my-app/src/components/UserPanel.js
+++ my-app/src/components/UserPanel.js
@@ -1,29 +1,5 @@
 
-import React, { useEffect, useState } from 'react';
-import '../App.css';
-import NavBar from './NavBar';
-import Footer from './Footer';
 
-function UserPanel() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const userLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-    setIsLoggedIn(userLoggedIn);
-  }, []);
-
-  return (
-    <>
-      <NavBar isLoggedIn={isLoggedIn} />
-      <section>
-        <div>{/* Twój kod dla UserPanel */}</div>
-      </section>
-      <Footer />
-    </>
-  );
-}
-
-=======
 import React, { useEffect, useState } from 'react';
 import '../App.css';
 import NavBar from './NavBar';
